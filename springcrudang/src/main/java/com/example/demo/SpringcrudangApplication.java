87e1613b7735877d2d369cac846cb5bd41bf16fd
kali <kali@DESKTOP-TH3UNTV>
2021-06-15 09:36:10 -0700

    new project structure

---
+++ springcrudang/src/main/java/com/example/demo/SpringcrudangApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcrudangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcrudangApplication.class, args);
+	}
+
+}
